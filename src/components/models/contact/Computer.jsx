f43b9320275f7e6bab00afeffdb9ffa3cfbfce87
Laiba-Zafar7 <zaafarlaiba@gmail.com>
2026-05-23 00:17:35 +0500

    Update contact section UI and fixes

--- src/components/models/contact/Computer.jsx
+++ src/components/models/contact/Computer.jsx
@@ -1,30 +1,13 @@
-import { useGLTF } from "@react-three/drei";
-
-export function Computer(props) {
-  const { nodes, materials } = useGLTF(
-    "/models/computer-optimized-transformed.glb"
-  );
-
+const ContactExperience = () => {
   return (
-    <group {...props} dispose={null}>
-      <group position={[-4.005, 67.549, 58.539]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Cube000_ComputerDesk_0001_1.geometry}
-          material={materials["ComputerDesk.001"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Cube000_ComputerDesk_0001_2.geometry}
-          material={materials["FloppyDisk.001"]}
-        />
-      </group>
-    </group>
+    <div className="w-full h-full flex items-center justify-center overflow-hidden">
+      <img
+        src="/images/computer.png"
+        alt="computer"
+        className="w-full h-full object-cover"
+      />
+    </div>
   );
-}
+};
 
-useGLTF.preload("/models/computer-optimized-transformed.glb");
-
-export default Computer;
+export default ContactExperience;
